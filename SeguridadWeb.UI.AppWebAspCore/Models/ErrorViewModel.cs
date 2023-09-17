0e3c609534ef32483c11a17046f17656ded5fd21
moise <moise@ADONAY>
2023-09-17 09:21:21 -0600

    Agregar archivos de proyecto.

---
+++ SeguridadWeb.UI.AppWebAspCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SeguridadWeb.UI.AppWebAspCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
