81f3e86a3748cda0bd86addae33072a345c5b973
Adonay <moisesbonilla212@gmail.com>
2023-09-17 15:04:23 -0600

    Programé la Entidad de Rol

--- SeguridadWeb.EntidadesDeNegocio/Usuario.cs
+++ SeguridadWeb.EntidadesDeNegocio/Usuario.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SeguridadWeb.EntidadesDeNegocio
 {
     public class Usuario
     {
-
+       
     }
 }
