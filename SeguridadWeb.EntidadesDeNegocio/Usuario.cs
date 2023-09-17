05f764c5c87d359b8ee876c56ccc0ef08e1bef47
Adonay <moisesbonilla212@gmail.com>
2023-09-17 16:06:22 -0600

    Programé la Entidad de Usuario

--- SeguridadWeb.EntidadesDeNegocio/Usuario.cs
+++ SeguridadWeb.EntidadesDeNegocio/Usuario.cs
@@ -11,6 +11,44 @@ namespace SeguridadWeb.EntidadesDeNegocio
 {
     public class Usuario
     {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Rol")]
+        [Required(ErrorMessage = "Rol es obligatorio")]
+        [Display(Name = "Rol")]
+        public int IdRol { get; set; }
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Apellido es ogligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "El login es obligatorio")]
+        [StringLength(25, ErrorMessage = "Maximo de 25 caracteres")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "El password es obligatorio")]
+        [DataType(DataType.Password)]
+        [StringLength(32, ErrorMessage = "Password debe estar entre 5 a 32 caracteres", MinimumLength = 5)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Estatus es ogligatorio")]
+        public byte Estatus { get; set; }
+        [Display(Name = "Fecha registro")]
+        public DateTime FechaRegistro { get; set; }
+        public Rol Rol { get; set; }
+        [NotMapped]
+        public int Top_aux { get; set; }
+        [Required(ErrorMessage = "Confirmar password")]
+        [StringLength(32, ErrorMessage = "Password debe estar entre 5 a 32 caracteres", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password y confirmar password deben de ser iguales")]
+        [Display(Name = "Confirmar password")]
+        public string ConfirmPassword_aux { get; set; }
+        
        
+    }
+    public enum Estatus_Usuario
+    {
+        ACTIVO = 1,
+        INACTIVO = 2
     }
 }
